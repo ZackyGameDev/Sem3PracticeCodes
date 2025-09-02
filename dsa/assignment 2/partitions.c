018a8b2b71376364ed38af17acf20f3ec80768ed
Aayan <zaacky2456@gmail.com>
2025-09-03 03:25:34 +0530

    getting closer!

--- dsa/assignment 2/partitions.c
+++ dsa/assignment 2/partitions.c
@@ -12,7 +12,7 @@ void print(const int *a, size_t n)
   printf("{");
   for (size_t i = 0; i < n; ++i)
     {
-        printf("%d ", a[i]);
+        printf("%d", a[i]);
         if (i != n-1) printf(", ");
     }
   printf("}\n");
@@ -52,41 +52,47 @@ void combinations (
 }
 
 
-void do_partitions(int *a, int n, int k, int k1) {
-    print(a, k1);
-
-
-    // we dont even need another array bro, n and k1 is enough
+void do_partitions(int *a, int n, int d, int k, int k1) {
+    
+    // we dont even need another array bro, d is enough
     // to keep track of the digit to place at position k1
+    // d here is the rank of the element to place at k1, 
+    // rank in the entire array. (d=0 means smallest number
+    // in the array, d=n-1 meaning the largest number in the array)
     if (k1 == 0) {
-        do_partitions(a, n, k, k1+1);
+        print(a, k1+1);
+        do_partitions(a, n, d+1, k, k1+1);
         return;
     }
 
     if (k1 > (n-1)-(k-1)) {
         return;
     }
+
+    if (d > n-1) { 
+        return;
+    }
+
     // this runs on the assumption that everything on the right
     // of k1 is sorted in ascending order. 
-    for (int i = k1; i < n; ++i) {
-        if (a[k1] <= a[i]) {
-            swap(&a[k1], &a[i]);
-            break;
-        }
-    } // now the k1 position should be increased to the next available digit.
+    swap(&a[k1], &a[d]);
+    print(a, k1+1);
     // since we replaced it at the first instance of greater number, the ascending order
     // of the right side of k1 should be preserved. 
 
     
-    do_partitions(a, n, k, k1+1);
-    do_partitions(a, n, k, k1);
+    do_partitions(a, n, d+1, k, k1+1);
+    do_partitions(a, n, d+1, k, k1);
     // by this point the k1'th element should be max(a)
     // so now to maintain the lexicographic nature of the array 
     // we need to bubble it back up to the max position 
     // before the arrway is returned to the parent function (the 
     // funciton that called this function in the recursion stack)
     while (k1 < n-1) {
-        swap(&a[k1], &a[k1+1]);
+        if (a[k1] > a[k1+1])
+            swap(&a[k1], &a[k1+1]);
+        else 
+            break;
         ++k1;
     }
 
@@ -98,7 +104,7 @@ void partitions(int n, int k) {
     int a[n];
     for (int i = 0; i < n; ++i) 
         a[i] = i+1;
-    do_partitions(a, n, k, 0);
+    do_partitions(a, n, 0, k, 0);
 }
 
 int main()
