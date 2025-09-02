bf344e53177276291a414bffcaa81ecc4abbe597
Aayan <zaacky2456@gmail.com>
2025-09-03 03:02:11 +0530

    また違うね。

--- dsa/assignment 2/partitions.c
+++ dsa/assignment 2/partitions.c
@@ -15,7 +15,7 @@ void print(const int *a, size_t n)
         printf("%d ", a[i]);
         if (i != n-1) printf(", ");
     }
-  printf("}");
+  printf("}\n");
 }
 
 void do_combinations (
@@ -52,38 +52,53 @@ void combinations (
 }
 
 
-void do_partitions(int *a, int n, int k, int k1, int k2) {
-    // we dont even need another array bro, n and k1-k2 is enough
+void do_partitions(int *a, int n, int k, int k1) {
+    print(a, k1);
+
+
+    // we dont even need another array bro, n and k1 is enough
     // to keep track of the digit to place at position k1
-    if (k1-k2 == 0) {
-        do_partitions(a, n, k, k1+1, k2);
+    if (k1 == 0) {
+        do_partitions(a, n, k, k1+1);
         return;
     }
 
-    if (k1-k2 > (n-1)-(k-1)) return;
-
+    if (k1 > (n-1)-(k-1)) {
+        return;
+    }
     // this runs on the assumption that everything on the right
     // of k1 is sorted in ascending order. 
-    for (int i = 0; i < n-k1; ++i)
-    swap(&a[k1], &a[d-1]);
-    do_partitions(a, n, d+1, k, k1+1);
-    do_partitions(a, n, d+1, k, k1);
+    for (int i = k1; i < n; ++i) {
+        if (a[k1] <= a[i]) {
+            swap(&a[k1], &a[i]);
+            break;
+        }
+    } // now the k1 position should be increased to the next available digit.
+    // since we replaced it at the first instance of greater number, the ascending order
+    // of the right side of k1 should be preserved. 
+
+    
+    do_partitions(a, n, k, k1+1);
+    do_partitions(a, n, k, k1);
     // by this point the k1'th element should be max(a)
     // so now to maintain the lexicographic nature of the array 
     // we need to bubble it back up to the max position 
     // before the arrway is returned to the parent function (the 
     // funciton that called this function in the recursion stack)
-    while (a[k1]-a[k1-1] != 1) {
-        swap(&a[k1], !a[k1+1]);
+    while (k1 < n-1) {
+        swap(&a[k1], &a[k1+1]);
         ++k1;
     }
+
+    return;
+
 }
 
 void partitions(int n, int k) {
     int a[n];
     for (int i = 0; i < n; ++i) 
         a[i] = i+1;
-    do_partitions(a, n, 1, k, 0);
+    do_partitions(a, n, k, 0);
 }
 
 int main()
